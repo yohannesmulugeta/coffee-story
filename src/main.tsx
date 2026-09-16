b96eead1229b4dcf0c44db8598295665c946610e
joni <70434578+yohannesmulugeta@users.noreply.github.com>
2026-09-16 19:54:19 +0300

    Load Sibu motion styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles/globals.css";
 import "./styles/sibu.css";
+import "./styles/sibu-motion.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
